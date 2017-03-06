59bcc9ecc2460187c1fe88a8126d8b26ba8e9b4e
zhanning.fan <zhanning.fan@pjbest.com>
2017-03-06 11:37:03 +0800

    add click alert and click element delete

--- binbin/js/task4.js
+++ binbin/js/task4.js
@@ -18,30 +18,34 @@ leftIn.onclick = function(){
 	if(isNum() !== 0){
 		var div = addElement();
 		showList.insertBefore(div,showList.childNodes[0]);
-
-
 	}
 }
 //右侧出
 rightOut.onclick = function(){
+	alert("删除的数字为" + showList.childNodes[showList.childNodes.length - 1].innerText);
 	showList.removeChild(showList.childNodes[showList.childNodes.length - 1]);
 }
 //左侧出
 leftOut.onclick = function(){
+	alert("删除的数字为" + showList.childNodes[0].innerText);
 	showList.removeChild(showList.childNodes[0]);
 }
 
 
-//根据输入值创建新的div
+
+//根据输入值创建新的div，并添加点击移除事件
 function addElement() {
 	var div = document.createElement('div');
 	div.innerHTML = input.value;
 	div.setAttribute('class','listItem');
-	return div;
-	
+	div.onclick = function(){
+		alert("删除的数字为" + this.innerText);
+		showList.removeChild(this);
+	}
+	return div;	
 }
 
-//验证输入的是否为合法的数字
+//验证输入的是否为合法的数字,只支持输入数字(包括小数、整数)
 function isNum(){
 	if(/^\d+(\.\d+)?$/.test(input.value) == false){
 		alert('请输入合法的数字');
